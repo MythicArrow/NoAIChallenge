4f659c7091168c87de5901c3a3261ce2b88dadc1
Mythereus <136450574+MythicArrow@users.noreply.github.com>
2025-04-17 23:11:04 +0200

    Solved issues

--- Day1phi.cpp
+++ Day1phi.cpp
@@ -2,8 +2,8 @@
 #include <iomanip>
 #include <cmath>
 int main(){
-  double golden = 1 + sqrt(5) / 2;
-  std::setprecision(10000);
+  double golden = (1 + sqrt(5)) / 2;
+  std::cout<<std::setprecision(10000);
   std::cout<<golden<<'\n';
   return 0;
 
