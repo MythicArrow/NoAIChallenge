86671d9f7beffa2bf9be8f38645e9917718faa4b
Mythereus <136450574+MythicArrow@users.noreply.github.com>
2025-04-17 23:07:19 +0200

    Create Day1phi.cpp

---
+++ Day1phi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+int main(){
+  double golden = 1 + sqrt(5) / 2;
+  std::setprecision(10000);
+  std::cout<<golden<<'\n';
+  return 0;
+
+
+
+
+} 
