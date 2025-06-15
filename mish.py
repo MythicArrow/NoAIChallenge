d849318ec3344833b8caa701bdf6caa657470028
Mythereus <136450574+MythicArrow@users.noreply.github.com>
2025-06-16 00:22:06 +0200

    Update mish.py

--- mish.py
+++ mish.py
@@ -1,6 +1,7 @@
 import jax
 import jax.numpy as jnp
 paper = 'https://arxiv.org/abs/1908.08681'
-def mish(x):
+def mish(x:ArrayLike) -> Array:
+    x = jnp.asarray(x)
     return x * jnp.tanh(jax.nn.softplus(x))
 # More info can be found on the paper
